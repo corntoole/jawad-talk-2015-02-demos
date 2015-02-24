52d2a58cd49421aa71c141e4145e4ea0f5df18bf
Cornelius Toole <cornelius.toole@gmail.com>
2015-02-24 05:51:38 -0600

    basic ng2 app skeleton seems in place

--- learn-ng/js/root-ex1-ng2.js
+++ learn-ng/js/root-ex1-ng2.js
@@ -1,4 +1,17 @@
 import {Component, TemplateConfig, bootstrap, Foreach} from 'angular2/angular2';
 import {bind} from 'angular2/di';
 
+@Component({
+  selector: 'hello-app',
+  componentServices: [],
+  template: new TemplateConfig({
+    inline: "<div>{{message}}</div>",
+    directives: []
+  })
+})
+class HelloApp {
 
+}
+export function main() {
+  bootstrap(HelloApp);
+}
