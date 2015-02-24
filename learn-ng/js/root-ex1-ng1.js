b2a995526d272ad243ae30944835e92d629c15a9
Cornelius Toole <cornelius.toole@gmail.com>
2015-02-24 05:33:28 -0600

    added dependencies for es6 transpilation
    via from http://github.com/davideast/ng2do

--- learn-ng/js/root-ex1-ng1.js
+++ learn-ng/js/root-ex1-ng1.js
@@ -1,4 +1,4 @@
 angular.module('root', [])
   .controller("index", function ($scope) {
-  	 $scope.message = "Hello Jxn Devs!";
+    $scope.message = "Hello Jxn Devs!";
   });
---
+++ learn-ng/js/root-ex1-ng2.js
@@ -0,0 +1,4 @@
+import {Component, TemplateConfig, bootstrap, Foreach} from 'angular2/angular2';
+import {bind} from 'angular2/di';
+
+
